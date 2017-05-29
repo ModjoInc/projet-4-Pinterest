99991dc9946138d3bf13a1638d485579176b3799
ModjoInc <habibem@gmail.com>
2017-05-29 15:28:42 +0200

    formulaire ver1

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Projet Pinterest</title>
+    <link rel="stylesheet" href="/css/style.css">
   </head>
   <body>
  <h1>PINTEREST-LIKE</h1>
+
+   <form action="upoad.php" method="post" enctype="multipart/form-data">
+     <p>upload image</p>
+     <p><input type="file" name="fileToUpload" id="fileToUpload"></p>
+     <p><input type="submit" value="Upload Image" name="submit"></p>
+   </form>
+
   </body>
 </html>
